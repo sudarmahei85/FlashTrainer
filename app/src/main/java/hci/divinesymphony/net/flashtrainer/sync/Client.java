6354bab1687ca589ef9a073250097706a4506e2d
Rick Houser <rick@divinesymphony.net>
2015-04-04 20:30:49 -0400

    Overhauled the helper functions that provide URL and file paths.

--- app/src/main/java/hci/divinesymphony/net/flashtrainer/sync/Client.java
+++ app/src/main/java/hci/divinesymphony/net/flashtrainer/sync/Client.java
@@ -3,8 +3,15 @@ package hci.divinesymphony.net.flashtrainer.sync;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.math.BigInteger;
 import java.util.UUID;
 
@@ -15,7 +22,8 @@ public class Client {
 
     private static final String CLIENT_PREFS = "client";
     private static final String KEY = "client_key";
-    private static final String BASE_URL = "base_url";
+    private static final String MEDIA_BASE_URL = "mediabase_url";
+    private static final String XML_BASE_URL = "xmlbase_url";
 
     private static final String MEDIA_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/AuPAIR/media/";
     private static final String TMP_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/AuPAIR/tmp/";
@@ -24,8 +32,8 @@ public class Client {
     private static Client client;
 
     private final String clientKey;
-    private final String baseUrl;
-
+    private final String mediaBaseUrl;
+    private final String xmlBaseUrl;
 
     public static Client initialize(Context ctx) {
         if (client == null) {
@@ -44,38 +52,55 @@ public class Client {
         SharedPreferences.Editor editor = null;
 
         String id = settings.getString(KEY, null);
-        String baseUrl = settings.getString(BASE_URL, null);
+        String mediaBaseUrl = settings.getString(MEDIA_BASE_URL, null);
+        String xmlBaseUrl = settings.getString(XML_BASE_URL, null);
 
         if (id == null) {
-            editor = settings.edit();
+            if (editor == null) {
+                editor = settings.edit();
+            }
             //generate new id
             id = UUID.randomUUID().toString();
             editor.putString(KEY, id);
+            Log.i(this.getClass().getName(), "generated a new client key:" + id);
         }
 
-        if (baseUrl == null) {
-            editor = settings.edit();
+        if (mediaBaseUrl == null) {
+            if (editor == null) {
+                editor = settings.edit();
+            }
             //store the default
-            baseUrl = "https://aupair.divinesymphony.net/downloads/";
-//TODO - this setting is temporarily disabled until we have working downloads via this address
-//            editor.putString(BASE_URL, baseUrl);
+            mediaBaseUrl = "http://aupair.divinesymphony.net/media/";
+//TODO once finalized, uncomment this to persist in the client
+//            editor.putString(MEDIA_BASE_URL, mediaBaseUrl);
+        }
+
+        if (xmlBaseUrl == null) {
+            if (editor == null) {
+                editor = settings.edit();
+            }
+            //store the default
+            xmlBaseUrl = "http://aupair.divinesymphony.net/xml/";
+//TODO once finalized, uncomment this to persist in the client
+//            editor.putString(XML_BASE_URL, xmlBaseUrl);
         }
 
         if (editor != null) {
-            editor.commit();
+            editor.apply();
         }
 
         this.clientKey = id;
-        this.baseUrl = baseUrl;
+        this.xmlBaseUrl = xmlBaseUrl;
+        this.mediaBaseUrl = mediaBaseUrl;
     }
 
     /**
-     * Return the base URL
-     * @return the base URL
+     * Return the media URL
+     * @return the media URL
      */
-    public String getBaseUrl() {
-        Log.v(this.getClass().getName(), "getBaseUrl() = "+this.baseUrl);
-        return this.baseUrl;
+    public String getMediaUrl() {
+        Log.v(this.getClass().getName(), "getMediaUrl() = "+this.mediaBaseUrl);
+        return this.mediaBaseUrl;
     }
 
     /**
@@ -94,7 +119,7 @@ public class Client {
     public String getUserKey() {
 //TODO - temporarily return the full key
         String str = this.getKey();
-//        String str = this.clientKey.substring(0,3)+'-'+this.clientKey.substring(4,7);
+//        String str = this.clientKey.substring(0,8);
         Log.v(this.getClass().getName(), "getUserKey() = "+str);
         return str;
     }
@@ -116,4 +141,62 @@ public class Client {
         Log.v(this.getClass().getName(), "getTmpPath() = "+TMP_PATH);
         return TMP_PATH;
     }
+
+    /**
+     * Return the XML Config URL
+     * @return the XML Config URL
+     */
+    public String getConfigUrl() {
+        Log.v(this.getClass().getName(), "getConfigUrl() = "+this.xmlBaseUrl+this.getKey());
+        return this.xmlBaseUrl+this.getKey();
+    }
+
+    public InputStream getConfigXMLTemp(Context ctx) {
+        return this.getConfigXML(ctx, true);
+    }
+
+    public InputStream getConfigXML(Context ctx) {
+        return this.getConfigXML(ctx, false);
+    }
+
+    private InputStream getConfigXML(Context ctx, boolean useTemp) {
+        InputStream is;
+        File configFile;
+        if (useTemp) {
+            configFile = new File(this.getMediaPath() + Client.getClient().getKey());
+        } else {
+            configFile = new File(this.getMediaPath() + "current.xml");
+            if (!configFile.exists()) {
+                InputStream template;
+                OutputStream out;
+                try {
+                    template = ctx.getAssets().open("current.xml");
+                    out = new FileOutputStream(configFile);
+                    byte[] buffer = new byte[1024];
+                    int len = template.read(buffer);
+                    while (len != -1) {
+                        out.write(buffer, 0, len);
+                        len = template.read(buffer);
+                    }
+                    template.close();
+                    out.close();
+                } catch (IOException e) {
+                    Log.e(this.getClass().getName(), "this is really bad");
+                    throw new RuntimeException("This is really bad", e);
+                }
+            }
+        }
+        try {
+//            if (configFile.exists()) {
+                is = new FileInputStream(configFile);
+//            } else {
+//                is = ctx.getAssets().open("current.xml");
+//            }
+        } catch (IOException e) {
+            Log.e(this.getClass().getName(), "Unable to read the xml config");
+            throw new RuntimeException("Unable to read the xml config", e);
+        }
+        return is;
+    }
+
 }
