787b755812317d97aa652902de205b41131cc781
Rick Houser <rick@divinesymphony.net>
2015-04-04 20:32:25 -0400

    Added a new config type, that can download files without having checksums.  Also, helper function to determine the fetch URL based on type.

--- app/src/main/java/hci/divinesymphony/net/flashtrainer/beans/DisplayItem.java
+++ app/src/main/java/hci/divinesymphony/net/flashtrainer/beans/DisplayItem.java
@@ -1,17 +1,28 @@
 package hci.divinesymphony.net.flashtrainer.beans;
 
+import hci.divinesymphony.net.flashtrainer.sync.Client;
+
 /**
  * Created by rick on 3/8/15.
  */
 public class DisplayItem {
 
-    public enum MediaType { TEXT, IMAGE, VIDEO, SOUND }
+    public enum MediaType { TEXT, IMAGE, VIDEO, SOUND, CONFIG }
 
     private final String text;
     private final String file;
     private final MediaType type;
     private final String id;
     private final String sha256;
+
+    public static DisplayItem getConfig() {
+        return new DisplayItem(
+                MediaType.CONFIG,
+                null,
+                Client.getClient().getConfigUrl(),
+                Client.getClient().getKey(),
+                null);
+    }
 
     public DisplayItem(String text, String id) {
         this(MediaType.TEXT, id, text, null, null);
@@ -30,8 +41,9 @@ public class DisplayItem {
         this.id = id;
         this.text = text;
         this.sha256 = sha256;
-        if ((type != MediaType.TEXT) && ( this.file == null||this.file.isEmpty() || this.sha256 == null|| this.sha256.isEmpty() )) {
-            throw new IllegalArgumentException("All display items but text must have a file and checksum present");
+        if ((type != MediaType.TEXT && type != MediaType.CONFIG) &&
+                ( this.file == null||this.file.isEmpty() || this.sha256 == null|| this.sha256.isEmpty() )) {
+            throw new IllegalArgumentException("All display items but text and config must have a file and checksum present");
         }
     }
 
@@ -44,6 +56,14 @@ public class DisplayItem {
     }
 
     public String getFile() { return this.file; }
+
+    public String getUrl() {
+        if (this.type != MediaType.CONFIG) {
+            return Client.getClient().getMediaUrl()+this.file;
+        } else {
+            return this.text;
+        }
+    }
 
     public String getSha256() { return this.sha256; }
 
