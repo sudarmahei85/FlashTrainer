25562ada5d2cf4f65a2581303467f7e628caae27
sudarmahei85 <smahendi@umflint.edu>
2015-04-05 20:27:23 -0500

    Response changes

--- app/src/main/java/hci/divinesymphony/net/flashtrainer/beans/Responses.java
+++ app/src/main/java/hci/divinesymphony/net/flashtrainer/beans/Responses.java
@@ -1,53 +1,60 @@
+package hci.divinesymphony.net.flashtrainer.beans;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
- * 
- * 
+ *
+ *
  * Response class that creates response object which I want to store the response group,
- * groupID, response text & responseID. It needs to be modified to 
+ * groupID, response text & responseID. It needs to be modified to
  */
 
 public class Responses {
 
-	private final String groupName;
-	private final String groupID;
-	private final List<String> responseID;
-	private final List<String> responseslist;
+    private String groupName;
+    private Integer groupID;
+    private HashMap<Integer,String> response;
 
-	public Responses(String groupName, String groupID, List<String> responseslist, List<String> responseID ){
-		this.groupName = groupName;
-		this.groupID = groupID;
-		this.responseslist = responseslist;
-		this.responseID = responseID;
 
-	}
-	
-	public String getGroupName(){
-		return this.groupName;
-	}
-	public String getGroupID(){
-		return this.groupID;
-	}
-	public List<String> getResponse(){
-		return this.responseslist;
-	}
-	public List<String> getResponseID(){
-		return this.responseID;
-	}
+    public Responses(){
+    }
 
-	public String toString() {
-		StringBuffer sb = new StringBuffer();
-		sb.append("Group Name: " + getGroupName());
-		sb.append("\n");
-		sb.append("Group ID: " + getGroupID());
-		sb.append("\n");
-		sb.append("Text: " + getResponse());
-		sb.append("\n");
-		sb.append("Item ID: " + getResponseID());
+    public Responses(String groupName, Integer groupID, HashMap<Integer,String> response){
+        this.groupName = groupName;
+        this.groupID = groupID;
+        this.response = response;
 
-		return sb.toString();
-	}
+    }
+
+    public String getGroupName(){
+        return groupName;
+    }
+
+    public Integer getGroupID() {
+        return groupID;
+    }
+
+    public HashMap<Integer, String> getResponse() {
+        return response;
+    }
+
+    public void setResponse(HashMap<Integer, String> response) {
+        this.response = response;
+    }
+
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("Group Name: " + getGroupName());
+        sb.append("\n");
+        sb.append("Group ID: " + getGroupID());
+        sb.append("\n");
+        sb.append("Text: " + getResponse());
+        sb.append("\n");
+
+        return sb.toString();
+    }
 
 
 }
