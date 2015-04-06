a02dee1d64965c60a804782a5fa5928ec4f6ba7e
sudarmahei85 <smahendi@umflint.edu>
2015-04-05 20:23:46 -0500

    Fixed the Random response

--- app/src/main/java/hci/divinesymphony/net/flashtrainer/backend/Selector.java
+++ app/src/main/java/hci/divinesymphony/net/flashtrainer/backend/Selector.java
@@ -14,17 +14,19 @@ import java.lang.String;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Random;
 
-import org.apache.commons.lang3.RandomStringUtils;
+
 
 import hci.divinesymphony.net.flashtrainer.beans.DisplayItem;
 import hci.divinesymphony.net.flashtrainer.beans.Problem;
 import hci.divinesymphony.net.flashtrainer.beans.ProblemSet;
+import hci.divinesymphony.net.flashtrainer.beans.Responses;
 
 /**
  * Created by wills_000 on 3/7/2015.
@@ -79,19 +81,46 @@ public class Selector
         return this.dom.getMedia();
     }
 
+    public List<Responses> getResponses() {
+        return this.dom.getResponseslist();
+    }
 
     //TODO - The answer needs to be worked on, the parsing is missing Response object.
     public List<DisplayItem> answerChoice(Problem problem)
     {
         //get these from the XML file instead
         List<DisplayItem> responses = new ArrayList<DisplayItem>(4);
-        responses.add(new DisplayItem(RandomStringUtils.randomAlphabetic(1), "45"));
-        responses.add(new DisplayItem(RandomStringUtils.randomAlphabetic(1), "af"));
-        responses.add(new DisplayItem(RandomStringUtils.randomAlphabetic(1), "4v"));
-        responses.add(new DisplayItem(RandomStringUtils.randomAlphabetic(1), "1m"));
+        List<Responses> responsesList = getResponses();
+        HashMap<Integer,String> response = new HashMap<Integer,String>();
+        for(Responses responses1:responsesList) {
+            if(responses1.getGroupID()==problem.getGroupId()){
+                response.putAll(responses1.getResponse());
+            }
+        }
+
+        List<Integer> keys = new ArrayList<Integer>();
+        keys.add(problem.getAnswerId());
+        int count=3;
+        for(int i=0; i<count;){
+          int val= answerchoicekey(response);
+           if(!keys.contains(val)){
+               keys.add(val);
+               i++;
+           }
+        }
+        Collections.shuffle(keys);
+        for(int j: keys) {
+            responses.add(new DisplayItem(response.get(j),String.valueOf(j)));
+        }
 
         return responses;
     }
 
+    public Integer answerchoicekey(HashMap<Integer,String> response){
 
+        Object[] responseKeys = response.keySet().toArray();
+        Integer key =(Integer)responseKeys[new Random().nextInt(responseKeys.length)];
+        return key;
+
+    }
 }
